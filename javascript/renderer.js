337d090f98a4e7af815c78ee99b0b0ba28472b97
mparaiso <aikah@free.fr>
2014-05-21 13:02:18 +0200

    setting menu

--- javascript/renderer.js
+++ javascript/renderer.js
@@ -1,57 +1,57 @@
+/*jslint node:true,browser:true*/
 /*global angular*/
 /**
  * @description playground the web tech playground
  * @copyright 2014 mparaiso <mparaiso@online.fr>
  * @license GPL
  */
+"use strict";
 angular.module('renderer', [])
-    .constant('RendererEvent', {
-        COMPILATION_ERROR: 'COMPILATION_ERROR',
-        RENDERER_ERROR: 'RENDERER_ERROR'
-    })
-    .service('RendererService', function($document, $rootScope, Compiler, RendererEvent) {
-        "use strict";
-        var iframe;
-        /**
-         * render an html string in an iframe
-         * @param {HTMLElement} output
-         * @param {editors} editors
-         */
-        this.renderHTML = function(output, editors) {
-            // console.log('compile');
-            var content, doc;
-            var _editors = angular.copy(editors);
-            try {
-                content = Compiler.compile(_editors);
-            } catch (error) {
-                console.error(error);
-                $rootScope.$broadcast(RendererEvent.COMPILATION_ERROR, error);
-            }
-            angular.element(iframe).remove();
-            iframe = angular.element('<iframe>', {
-                sandbox: 'allow-same-origin allow-scripts',
-                frameborder: '0'
-            }).get(0);
-            output.appendChild(iframe);
-            doc = iframe.contentDocument || iframe.contentWindow || iframe.document;
-            doc.open();
-            iframe.contentWindow.addEventListener('error', function(e) {
-                /** listen for errors in the iframe @see  http://stackoverflow.com/questions/6327128/can-i-catch-exception-of-iframe-in-parent-window-of-iframe */
-                $rootScope.$broadcast(RendererEvent.RENDERER_ERROR, [e.lineno, ':', e.colno, ' ', e.message].join(''));
-            },false);
-            doc.writeln(content);
-            doc.close();
-            //sconsole.clear();
-            console.log(content);
-        };
-    }).directive('renderer', function(RendererService) {
-        "use strict";
-        return {
-            restrict: 'A',
-            link: function($scope, el, attr) {
-                $scope.$on('run', function(event, editors) {
-                    RendererService.renderHTML(el.get(0), editors);
-                });
-            }
-        };
-    });
+.constant('RendererEvent', {
+    COMPILATION_ERROR: 'COMPILATION_ERROR',
+    RENDERER_ERROR: 'RENDERER_ERROR'
+})
+.service('RendererService', function($document, $rootScope, Compiler, RendererEvent) {
+    var iframe;
+    /**
+    * render an html string in an iframe
+    * @param {HTMLElement} output
+    * @param {editors} editors
+    */
+    this.renderHTML = function(output, editors) {
+        // console.log('compile');
+        var content, doc;
+        var _editors = angular.copy(editors);
+        try {
+            content = Compiler.compile(_editors);
+        } catch (error) {
+            console.error(error);
+            $rootScope.$broadcast(RendererEvent.COMPILATION_ERROR, error);
+        }
+        angular.element(iframe).remove();
+        iframe = angular.element('<iframe>', {
+            sandbox: 'allow-same-origin allow-scripts',
+            frameborder: '0'
+        }).get(0);
+        output.appendChild(iframe);
+        doc = iframe.contentDocument || iframe.contentWindow || iframe.document;
+        doc.open();
+        iframe.contentWindow.addEventListener('error', function(e) {
+            /** listen for errors in the iframe @see  http://stackoverflow.com/questions/6327128/can-i-catch-exception-of-iframe-in-parent-window-of-iframe */
+            $rootScope.$broadcast(RendererEvent.RENDERER_ERROR, [e.lineno, ':', e.colno, ' ', e.message].join(''));
+        },false);
+        doc.writeln(content);
+        doc.close();
+        //sconsole.clear();
+        console.log(content);
+    };
+}).directive('renderer', function(RendererService) {
+    return {
+        restrict: 'A',
+        link: function($scope, el, attr) {
+            $scope.$on('run', function(event, editors) {
+                RendererService.renderHTML(el.get(0), editors);
+            });
+        }
+    };
+});
--- test/unit/api.parse.test.js
+++ test/unit/api.parse.test.js
@@ -1,51 +1,69 @@
 /*global describe,beforeEach,inject,expect,spyOn,jasmine,angular,module,it,beforeEach*/
-xdescribe("api.parse", function() {
-	"use strict";
-	console.log("jasmine version", jasmine.version);
-	beforeEach(function() {
-		angular.module('test', ['api.parse', 'Parse.mock']);
-		module('test');
-	});
-	describe("Gist", function() {
-		beforeEach(function() {
-			var self = this;
-			inject(function(Gist) {
-				self.Gist = Gist;
-			});
-		});
-		describe("findLatestGists", function() {
-			it('should execute', function() {
-				this.Gist.findLatestGists();
-			});
-		});
-		describe("findGistById", function() {
-			it('should execute', function() {
-				var id = "foo";
-				this.Gist.findGistById(id);
-			});
-		});
-	});
-	describe("User", function() {
-		beforeEach(function() {
-			var self = this;
-			module('test');
-			inject(function(User, Parse) {
-				self.Parse = Parse;
-				self.User = User;
-			});
-		});
-		describe('getCurrentUser', function() {
-			it('should be undefined', function() {
-				expect(this.User.getCurrentUser()).toBeUndefined();
-				expect(this.Parse.User.current).toHaveBeenCalled();
-			});
-		});
-		describe("isAuthenticated", function() {
-			it('should be false', function() {
-				this.Parse.User.authenticated.and.returnValue(false);
-				expect(this.User.isAuthenticated()).toBeFalsy();
-				expect(this.Parse.User.authenticated).toHaveBeenCalled();
-			});
-		});
-	});
+describe("api.parse", function() {
+    "use strict";
+    //console.log("jasmine version", jasmine.version);
+    beforeEach(function() {
+        spyOn(Parse,'_ajax');
+        spyOn(Parse,'initialize');
+        Parse._ajax.and.callFake(function(){
+            console.log('ajax',arguments);
+        });
+        Parse.initialize.and.callThrough(function(){
+            console.log('initialize',arguments);
+        })
+        module('api.parse');
+        inject(function($injector){
+            self.$injector=$injector;
+        });
+    });
+    describe("Gist", function() {
+
+        beforeEach(function() {
+
+            this.Gist=this.$injector.get('Gist');   
+            this.$rootScope=this.$injector.get('$rootScope');
+            this.$timeout=this.$injector.get('$timeout');
+        });
+        describe("findLatest", function() {
+            it('should execute', function(done) {
+                this.Gist.findLatest().then(done).catch(function(err){
+                    console.log('fail',arguments);
+                    done();
+                });
+                this.$rootScope.$apply();
+                this.$timeout.flush();
+            });
+        });
+        xdescribe("findGistById", function() {
+            it('should execute', function() {
+                var id = "foo";
+                this.Gist.findGistById(id);
+            });
+        });
+    });
+    xdescribe("User", function() {
+        beforeEach(function() {
+            var self = this;
+            module('test');
+            inject(function(User, Parse) {
+                self.Parse = Parse;
+                self.User = User;
+            });
+        });
+        describe('getCurrentUser', function() {
+            it('should be undefined', function() {
+                spyOn(this.Parse.User,'current');
+                expect(this.User.getCurrentUser()).toBeUndefined();
+                expect(this.Parse.User.current).toHaveBeenCalled();
+            });
+        });
+        xdescribe("isAuthenticated", function() {
+            it('should be false', function() {
+                spyOn(this.Parse.User.prototype,'authenticated');
+                this.Parse.User.prototype.authenticated.and.returnValue(false);
+                expect(this.User.isAuthenticated()).toBeFalsy();
+                //expect(this.Parse.User.prototype.authenticated).toHaveBeenCalled();
+            });
+        });
+    });
 });
